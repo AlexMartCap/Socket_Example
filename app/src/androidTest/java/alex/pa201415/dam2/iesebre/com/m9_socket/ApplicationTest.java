2d0277ba1e2e312520bc089ce8db633e3b8ab827
Alexandre <alexmart4@gmail.com>
2015-03-16 20:20:11 +0100

    Socket example basics

---
+++ app/src/androidTest/java/alex/pa201415/dam2/iesebre/com/m9_socket/ApplicationTest.java
@@ -0,0 +1,13 @@
+package alex.pa201415.dam2.iesebre.com.m9_socket;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
